84d649f7ccbde5967043a00531e8df3c365fd8a2
root <root@localhost.localdomain>
2018-02-15 19:14:37 +0900

    express hello world

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
-var express = require("express");
-var app = express();
+const app = require("express")();
+const http = require('http').Server(app);
 
-/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
 var server = app.listen(1337, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
