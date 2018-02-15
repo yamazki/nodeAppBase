22f06b3843fcac0a078c275114deade322c6348b
root <root@localhost.localdomain>
2018-02-15 18:44:13 +0900

    express setting finish

---
+++ app.js
@@ -0,0 +1,9 @@
+/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
+var express = require("express");
+var app = express();
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+var server = app.listen(1337, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
